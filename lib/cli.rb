9fe02b92bb5ef2c08212045e29ed5b81ac1e4388
gnfisher <me@gnfisher.com>
2016-03-22 10:33:26 -0300

    stubbed #list_leagues functionality

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,6 +1,39 @@
 class FootballNow::CLI
 
   def call
-    puts "Hello world!"
+    welcome_screen
+
+    case get_user_input
+    when "exit"
+      goodbye_message
+    when "list leagues"
+      list_leagues
+    end
+
+  end
+
+  def list_leagues
+    puts (<<-EOT)
+    1. Premier Leagues
+    2. Primera Division
+    3. Bundesliga
+    4. Seria A
+    EOT
+  end
+
+  def welcome_screen
+    puts "Welcome to Football Now"
+    puts "You can:"
+    puts " - type `list leagues` to list all leagues."
+    puts " - type `exit` to quit now."
+  end
+
+  def goodbye_message
+    puts "Until next time!"
+    exit 0
+  end
+
+  def get_user_input
+    gets.strip
   end
 end
