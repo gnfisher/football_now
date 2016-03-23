b7bc5c18403a9671a06c690788c98730466a26f9
gnfisher <me@gnfisher.com>
2016-03-23 11:22:37 -0300

    Team attr_accessor and empty tests

---
+++ lib/team.rb
@@ -0,0 +1,8 @@
+class FootballNow::Team
+
+  attr_accessor :name, :league, :matches, :wins, :losses, :draws,
+   :goals_for, :goals_against, :standing_in_league
+
+  def initialize
+  end
+end
---
+++ spec/team_spec.rb
@@ -0,0 +1,4 @@
+require 'spec_helper'
+
+describe 'FootballNow::Team' do
+end
