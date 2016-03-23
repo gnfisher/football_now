dc007b3d5f3598eef9dee003e01d0f4eec30d1f3
gnfisher <me@gnfisher.com>
2016-03-23 11:44:10 -0300

    team/league relationship tests passing

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
 describe 'FootballNow::Team' do
+
+  describe '#league=' do
+    it 'assigns league to team and adds itself to the leagues array' do
+      league = FootballNow::League.new("Premier League", "www.league.com")
+      team = FootballNow::Team.new(name: "Liverpool")
+      team.league = league
+
+      expect(league.teams).to include(team)
+      expect(team.league).to eq(league)
+    end
+  end
 end
