fb06e883b47f87ff3e7e002bf51d3133ee44f622
gnfisher <me@gnfisher.com>
2016-03-23 16:17:22 -0300

    refactor

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,47 +1,41 @@
 class FootballNow::Scraper
   extend Capybara::DSL
 
+  # Put these in config later
   BASE_URL = "http://www.soccer24.com"
   LEAGUES = ["Premier League", "Primera Division", "Bundesliga", "Serie A"]
 
-  def self.scrape_leagues(leagues_url)
-    page = Nokogiri::HTML(open(BASE_URL))
+  def self.scrape_leagues
+    league_page = Nokogiri::HTML(open(BASE_URL))
     leagues = []
 
-    top_leagues = page.css('.left-menu').first.css('ul li')
-
-    top_leagues.each do |menu_element|
-      link_href = menu_element.css('a').attribute('href').value
-      league = menu_element.css('a').text
-      LEAGUES.each do |l|
-        leagues << collect_league_data(league, link_href) if l[/#{league}/]
+    league_page.css('.left-menu').first.css('ul li').each do |row|
+      link_href = row.css('a').attribute('href').value
+      league = row.css('a').text
+      LEAGUES.each do |lg|
+        leagues << {name: league, league_url: "#{BASE_URL}#{link_href}"} if lg[/#{league}/]
       end
     end
 
     leagues
   end
 
-  def self.collect_league_data(league, link_href)
-    { name: league, league_url: "#{BASE_URL}#{link_href}"}
-  end
-
   def self.scrape_teams(league_url)
-    visit(get_standings_page_url)
+    visit(get_standings_page_url(league_url))
     standings_page = Nokogiri::HTML(page.html)
-    all_teams_rows = standings_page.css('table#table-type-1 tbody tr')
 
     teams = []
 
-    all_teams_rows.each do |row|
+    standings_page.css('table#table-type-1 tbody tr').each do |row|
       goals_for_against = row.css('.goals').first.text.split(':')
       team_hash = {
-        name: row.css('.participant_name .team_name_span').text,
-        wins: row.css('.wins').text,
-        draws: row.css('.draws').text,
-        losses: row.css('.losses').text,
+        name:               row.css('.participant_name .team_name_span').text,
+        wins:               row.css('.wins').text,
+        draws:              row.css('.draws').text,
+        losses:             row.css('.losses').text,
         standing_in_league: row.css('.rank').text.chomp('.'),
-        goals_for: goals_for_against[0],
-        goals_against: goals_for_against[1]
+        goals_for:          goals_for_against[0],
+        goals_against:      goals_for_against[1]
       }
 
       teams << team_hash
@@ -50,7 +44,7 @@ class FootballNow::Scraper
     teams
   end
 
-  def self.get_standings_page_url
+  def self.get_standings_page_url(league_url)
     league_page = Nokogiri::HTML(open(league_url))
     standings_page_href = league_page.css('.page-tabs .ifmenu li a:contains("Standings")').attribute('href').value
     "#{BASE_URL}#{standings_page_href}"
--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -3,8 +3,7 @@ require 'spec_helper'
 describe 'FootballNow::Scraper' do
   describe '.scrape_leagues' do
     it 'creates array of League objects from url' do
-      league_url = "http://www.soccer24.com"
-      leagues = FootballNow::Scraper.scrape_leagues(league_url)
+      leagues = FootballNow::Scraper.scrape_leagues
 
       expect(leagues.first[:name]).to eq("Premier League")
       expect(leagues.count).to eq(FootballNow::Scraper::LEAGUES.count)
