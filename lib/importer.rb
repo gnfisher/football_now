8787d1e6403b6c831908b6ff517d74d16e1eb04b
gnfisher <me@gnfisher.com>
2016-03-25 10:39:55 -0300

    teams generating

--- lib/importer.rb
+++ lib/importer.rb
@@ -3,6 +3,14 @@ class FootballNow::Importer
   def self.generate
     create_leagues
     create_teams
+    binding.pry
+    #create_matches
+  end
+
+  def self.create_leagues
+    FootballNow::Scraper.scrape_leagues.each do |league|
+      FootballNow::League.create_from_hash(league)
+    end
   end
 
   def self.create_teams
@@ -17,9 +25,11 @@ class FootballNow::Importer
     FootballNow::Scraper.scrape_teams(league_url)
   end
 
-  def self.create_leagues
-    FootballNow::Scraper.scrape_leagues.each do |league|
-      FootballNow::League.create_from_hash(league)
+  def self.create_matches
+    FootballNow::League.all.each do |league|
+      matches_hash(league.league_url).each do |match_hash|
+        FootballNow::Team.create_from_hash(team_hash)
+      end
     end
   end
 end
