7745c3c9f1792eff1c50d4b2fa3eb35f2b2895d3
gnfisher <me@gnfisher.com>
2016-03-25 10:21:34 -0300

    Importer loads leagues

--- lib/importer.rb
+++ lib/importer.rb
@@ -1,4 +1,12 @@
 class FootballNow::Importer
-  # Importer will create all our Leagues, Teams and Matches.
-  # Will use the Scraper.
+
+  def self.generate
+    create_leagues
+  end
+
+  def self.create_leagues
+    FootballNow::Scraper.scrape_leagues.each do |league|
+      FootballNow::League.create_from_hash(league)
+    end
+  end
 end
